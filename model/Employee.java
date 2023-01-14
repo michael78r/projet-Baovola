0ec105c10ed6fd96b4268b02f68ee7c78ecd805d
Michaël <121096401+michael78r@users.noreply.github.com>
2023-01-14 20:57:04 +0300

    Create Employee.java
    model employee

---
+++ model/Employee.java
@@ -0,0 +1,20 @@
+package model;
+public class Employee{
+
+    String nom;
+    String prenom;
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+
+}
